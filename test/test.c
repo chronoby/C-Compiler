1b0f07c0be169c234ced42d451a2976186a8e403
Cyanbox <obama1205@yeah.net>
2021-06-13 21:39:38 +0800

    UPDATE update.log

--- test/test.c
+++ test/test.c
@@ -1,14 +1,20 @@
 int x = 12 + 6 * 5 - 4; 
-int y = 0 - 4;
+int y = 2;
 
 int plus(int a, int b)
 {
-    return a + b;
+    return a * b;
+}
+
+int minus(int a, int b)
+{
+    return a - b;
 }
 
 int main()
 {
     int ans = plus(x, y);
-    
-    return ans;
+    ans = minus(x, y);
+
+    return ans + 5;
 }
